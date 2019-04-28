7c2bc6e47caf439f3ee6b57b30b38ada9e8e8865
josephmuema <jmuema95@gmail.com>
2019-04-28 13:48:21 +0300

    Added code needed to add new quote and toggle effect

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quotes',
@@ -7,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesComponent implements OnInit {
 
+  quotes = [
+    new Quote(1, "A Journey of a thousand miles begins with one step", "Joseph",0, 0, "Muema"),
+    new Quote(2, "If you argue with a fool you become one", "James", 0, 0,"Sila")
+  ]
+  toggleDetails(index) {
+    this.quotes[index].showDetails = !this.quotes[index].showDetails;
+  }
+  deleteQuote(isDelete, index) {
+
+    if (isDelete) {
+      let toDelete = confirm("To go ahead please click ok, otherwise cancel.");
+      if (toDelete) {
+        this.quotes.splice(index, 1)
+      }
+    }
+  }
+  addNewQuote(quote){
+    if (quote.enterQuote == "" || quote.author == "" || quote.submitter == "") {
+    alert("Empty field");
+  }
+  else {
+    let quoteLength = this.quotes.length;
+        quote.id=quoteLength+1;
+        quote.submitDate = new Date();
+        this.quotes.push(quote);
+}
+}
+   plusVote(isPlus, index) {
+    if (isPlus) {
+      this.quotes[index].upVote += 1;
+    }
+  }
+
+  minusVote(isMinus, index) {
+    if (isMinus) {
+      this.quotes[index].downVote += 1;
+    }
+  }
+
   constructor() { }
+
 
   ngOnInit() {
   }
-
 }
