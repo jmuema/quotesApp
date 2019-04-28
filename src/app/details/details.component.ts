74e31c86a5adf685bc608d6024c0b65ef12e8333
josephmuema <jmuema95@gmail.com>
2019-04-28 13:57:12 +0300

    Added code to dele and add/minus votes

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input,Output,EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
+
 
 @Component({
   selector: 'app-details',
@@ -7,6 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
+  @Input() quote:Quote;
+  
+  @Output() isDelete = new EventEmitter<boolean>();
+  @Output() isPlus = new EventEmitter<boolean>();
+  @Output() isMinus = new EventEmitter<boolean>();
+  
+  quoteDelete(remove:boolean) {
+    this.isDelete.emit(remove);
+  }
+  votePlus(plus:boolean) {
+    this.isPlus.emit(plus);
+  }
+  voteMinus(minus:boolean) {
+    this.isMinus.emit(minus);
+  }
   constructor() { }
 
   ngOnInit() {
