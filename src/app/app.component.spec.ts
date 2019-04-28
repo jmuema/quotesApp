754e290ffac7b6123de9d2fca85623c89d8c22d9
josephmuema <jmuema95@gmail.com>
2019-04-28 13:29:05 +0300

    Created count typescript file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'quotesApp2'`, () => {
+  it(`should have as title 'quotesApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('quotesApp2');
+    expect(app.title).toEqual('quotesApp');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to quotesApp2!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to quotesApp!');
   });
 });
