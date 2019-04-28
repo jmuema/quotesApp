b5e917a7b43ddaaa6e3e242eb9da59bf20a9cece
josephmuema <jmuema95@gmail.com>
2019-04-28 12:46:50 +0300

    Imported various components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailsComponent } from './details/details.component';
+import { FormsComponent } from './forms/forms.component';
+import { QuotesComponent } from './quotes/quotes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsComponent,
+    FormsComponent,
+    QuotesComponent
   ],
   imports: [
     BrowserModule,
