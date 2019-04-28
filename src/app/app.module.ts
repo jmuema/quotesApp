ea6f70e269f7f7e2e7f501801eedbd05bd352f32
josephmuema <jmuema95@gmail.com>
2019-04-28 13:25:29 +0300

    Imported the forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DetailsComponent } from './details/details.component';
 import { FormsComponent } from './forms/forms.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { CountPipePipe } from './count-pipe-pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     DetailsComponent,
     FormsComponent,
-    QuotesComponent
+    QuotesComponent,
+    CountPipePipe,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
